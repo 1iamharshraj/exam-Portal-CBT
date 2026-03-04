2436ada100ec1e49999cc4208246beb24889f15b
Harsh Raj <hraj20000@gmail.com>
2026-03-04 21:27:32 +0530

    fix: revert vite outDir hack, use TURBO_DISABLED env var instead

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -4,9 +4,6 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: process.env.VERCEL ? path.resolve(__dirname, '../../dist') : 'dist',
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
